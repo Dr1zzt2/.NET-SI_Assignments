40f20894052d9c28cfcf72b5dfb47bdd76ec70d2
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-05-23 20:34:11 +0200

    commit fix

---
+++ CreateClass/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public enum Gender { Male, Female }
+    public class Person
+    {
+        public String Name { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public Gender Gender { get; private set; }
+
+        public Person(string name, DateTime birthDate, string gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Gender = (Gender)Enum.Parse(typeof(Gender), gender);
+        }
+        public override string ToString()
+        {
+            return "Person: " + Name + " " + BirthDate + " " + Gender;
+        }
+    }
+}
---
+++ CreateClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person potatoMan = new Person("János a krumpliember", DateTime.Now, "Male");
+            Console.WriteLine(potatoMan.ToString());
+            Console.ReadKey();
+        }
+    }
+}
