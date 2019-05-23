405d2d14fc3454caadf882ddd2712e13f25708f7
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-05-23 22:13:47 +0200

    added string splitting and sorting

---
+++ SortString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Thanks for all the fish!";
+            string[] words = str.Split(' ');
+            Array.Sort(words);
+            string sorted = string.Join("X", words);
+            Console.WriteLine(sorted);
+            Console.ReadKey();
+        }
+    }
+}
