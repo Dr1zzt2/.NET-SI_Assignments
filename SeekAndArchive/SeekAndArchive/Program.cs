150547f83ee9d0e0f60a1a1a8317fd4f9ad8c4ba
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-06-03 19:44:21 +0200

    added application with file search functionality

---
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedName = args[0];
+            DirectoryInfo currentDirectory = new DirectoryInfo(args[1]);
+            Searcher searcher = new Searcher();
+            List<FileInfo> files = searcher.Search(currentDirectory, searchedName);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.DirectoryName + "\\" + file.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SeekAndArchive/SeekAndArchive/Searcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Searcher
+    {
+        private List<FileInfo> files = new List<FileInfo>();
+
+        public List<FileInfo> Search(DirectoryInfo currentDirectory, string searchedName)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles())
+            {
+                if (searchedName == file.Name)
+                    files.Add(file);
+            }
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                Search(dir, searchedName);
+            }
+            return files;
+        }
+    }
+}
