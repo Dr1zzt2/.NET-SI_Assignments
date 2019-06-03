3890f4ae6e98438c15f2fc7d728bb5a1d4a57f47
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-06-03 20:13:03 +0200

    added functionality for checking extensions if applicable

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -12,15 +12,17 @@ namespace SeekAndArchive
         static void Main(string[] args)
         {
             string searchedName = args[0];
+            string extension = null;
             if (searchedName.Contains('.'))
             {
                 string[] strings = searchedName.Split('.');
                 searchedName = strings[0];
+                extension = strings[1];
             }
 
             DirectoryInfo currentDirectory = new DirectoryInfo(args[1]);
             Searcher searcher = new Searcher();
-            List<FileInfo> files = searcher.Search(currentDirectory, searchedName);
+            List<FileInfo> files = searcher.Search(currentDirectory, searchedName, extension);
             foreach (FileInfo file in files)
             {
                 Console.WriteLine(file.DirectoryName + "\\" + file.Name);
--- SeekAndArchive/SeekAndArchive/Searcher.cs
+++ SeekAndArchive/SeekAndArchive/Searcher.cs
@@ -11,16 +11,16 @@ namespace SeekAndArchive
     {
         private List<FileInfo> files = new List<FileInfo>();
 
-        public List<FileInfo> Search(DirectoryInfo currentDirectory, string searchedName)
+        public List<FileInfo> Search(DirectoryInfo currentDirectory, string searchedName, string extension)
         {
             foreach (FileInfo file in currentDirectory.GetFiles())
             {
-                if (file.Name.Contains(searchedName))
+                if (file.Name.Contains(searchedName) && file.Extension == '.' + extension || extension == null) 
                     files.Add(file);
             }
             foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
             {
-                Search(dir, searchedName);
+                Search(dir, searchedName, extension);
             }
             return files;
         }
